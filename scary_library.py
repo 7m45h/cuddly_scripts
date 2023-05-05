a64cf9af58f32305aad996472ac42ee237c20cae
tmash <132013534+7m45h@users.noreply.github.com>
2023-05-05 16:48:14 +0530

    aaaaaaaaaa

--- scary_library.py
+++ scary_library.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python
+
+import json
+
+input_file = "/home/tmash/workspace/dev/harbor/bookmarks.json"
+
+with open(input_file, "r") as file:
+    data = json.loads(file.read())
+    print(json.dumps(data["children"][0]["children"], indent=4))
