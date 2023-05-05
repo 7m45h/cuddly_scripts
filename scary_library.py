1f639a77946ee1779fa0317fc813e924a7e7c4e9
tmash <132013534+7m45h@users.noreply.github.com>
2023-05-05 17:42:50 +0530

    it works

---
+++ scary_library.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from html.parser import HTMLParser
+
+input_file = "/home/tmash/workspace/dev/harbor/bookmarks.html"
+urls = ""
+
+class mrParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        for attr in attrs:
+            if attr[0] == "href":
+                print(attr[1])            
+
+parser = mrParser()
+
+with open(input_file, "r") as file:
+    parser.feed(file.read())
