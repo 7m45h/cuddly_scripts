83c8ddc419334687270168b7e446247c5210f555
tmash <132013534+7m45h@users.noreply.github.com>
2023-05-09 19:24:57 +0530

    why

--- scary_library.py
+++ scary_library.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
+from urllib.parse import urlparse
 from html.parser import HTMLParser
 
 input_file = "/home/tmash/workspace/dev/warehouse/bookmarks.html"
-urls = ""
 
 class mrParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         for attr in attrs:
             if attr[0] == "href":
-                print(attr[1])            
+                print(f"""
+<div class="div-link-main">
+    <img class="div-link-fav" src="https://www.google.com/s2/favicons?domain={urlparse(attr[1]).hostname}&sz=128" loading="lazy"/>
+    <a class="div-link-name" href="{attr[1]}">{attr[1]} &#8599;</a>
+</div>                
+""")
 
 parser = mrParser()
 
