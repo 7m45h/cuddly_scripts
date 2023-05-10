fea02872249804070c972b5eec29fe900edf07ef
tmash <132013534+7m45h@users.noreply.github.com>
2023-05-10 12:26:26 +0530

    remove update option

--- evil_list.py
+++ evil_list.py
@@ -5,7 +5,7 @@ import sqlite3
 import argparse
 
 parser = argparse.ArgumentParser(description="manage evil databse")
-parser.add_argument("mode", choices=["l", "a", "u", "g", "d", "n"], help="l: list all a: add new u: update g: get info d: delete n: new table")
+parser.add_argument("mode", choices=["l", "a", "g", "d", "n"], help="l: list all a: add new g: get info d: delete n: new table")
 args = parser.parse_args()
 
 con = sqlite3.connect("../warehouse/evil.db")
@@ -14,46 +14,36 @@ cur = con.cursor()
 def newDatabase():
     cur.execute("CREATE TABLE IF NOT EXISTS torrents(hash TEXT, name TEXT, isMovie INTEGER, year INTEGER, imdb TEXT)")
     con.commit()
-    
     print("[!] done")
 
 def addNew():
     print("[?]")
     hash = input("    hash: ").upper()
-    
     if cur.execute("SELECT hash FROM torrents WHERE hash=?", (hash,)).fetchone() is None:
         name = input("    name: ").translate(str.maketrans(string.punctuation + string.whitespace, "_"*(len(string.punctuation) + len(string.whitespace)))).lower()
         movi = input("    movi: ")
-        
         if movi == 1 or movi == "1":
             year = input("    year: ")
             imdb = input("    imdb: ")
-            
             print("[!] summary")
             print(f"    hash: {hash}")
             print(f"    name: {name}")
             print(f"    year: {year}")
             print(f"    imdb: {imdb}")
-            
             write = input("[?] write (y/n): ")
-            
             if write == "y":
                 cur.execute("INSERT INTO torrents VALUES (?, ?, ?, ?, ?)", (hash, name, movi, year, imdb))
                 con.commit()
-        
         elif movi == 0 or movi == "0":
             print("[!] summary")
             print(f"    hash: {hash}")
             print(f"    name: {name}")
-            
             write = input("[?] write (y/n): ")
-            
             if write == "y":
                 cur.execute("INSERT INTO torrents VALUES (?, ?, ?, ?, ?)", (hash, name, movi, "", ""))
                 con.commit()
         else:
             print("\n[!] invalid input")
-    
     else:
         print("\n[!] torrent allready exists")
 
@@ -67,7 +57,6 @@ def deleteRow():
     print("[!]")
     print(f"[{row[0]}]\t{row[1]}\t{row[2]}\t{row[3]}\t{row[4]}\t{row[5]}")
     delete = input("[?] delete (y/n): ")
-    
     if delete == "y":
         cur.execute("DELETE FROM torrents WHERE rowid=?", (id,))
         con.commit()
@@ -76,8 +65,6 @@ if (args.mode == "n"):
     newDatabase()
 elif (args.mode == "d"):
     deleteRow()
-elif (args.mode == "u"):
-    print("update")
 elif (args.mode == "a"):
     addNew()
 elif (args.mode == "g"):
