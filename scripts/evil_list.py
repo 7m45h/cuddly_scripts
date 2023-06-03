4d644d74edcbbd8ea39f0c0296f5d4e5df17689f
tmash <132013534+7m45h@users.noreply.github.com>
2023-06-03 13:32:13 +0530

    chage some stuff

--- scripts/evil_list.py
+++ scripts/evil_list.py
@@ -12,54 +12,57 @@ con = sqlite3.connect("../warehouse/evil.db")
 cur = con.cursor()
 
 def newDatabase():
-    cur.execute("CREATE TABLE IF NOT EXISTS torrents(hash TEXT, name TEXT, isMovie INTEGER, year INTEGER, imdb TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS movies(imdb TEXT PRIMARY KEY ASC, name TEXT, year INTEGER, hash TEXT NULL, poster BLOB NULL)")
     con.commit()
     print("[!] done")
 
 def addNew():
     print("[?]")
-    hash = input("    hash: ").upper()
-    if cur.execute("SELECT hash FROM torrents WHERE hash=?", (hash,)).fetchone() is None:
-        name = input("    name: ").translate(str.maketrans(string.whitespace, "_"*len(string.whitespace), string.punctuation)).lower()
-        movi = input("    movi: ")
-        if movi == 1 or movi == "1":
-            year = input("    year: ")
-            imdb = input("    imdb: ")
-            print("[!] summary")
-            print(f"    hash: {hash}")
-            print(f"    name: {name}")
-            print(f"    year: {year}")
-            print(f"    imdb: {imdb}")
-            write = input("[?] write (y/n): ")
+    imdb = input("    imdb: ")
+    existing_row = cur.execute("SELECT imdb, name, year FROM movies WHERE imdb=?", (imdb,)).fetchone()
+    if row is None:
+        name = input("    name: ")
+        year = input("    year: ")
+        hash = input("    hash: ")
+        poster = input("    poster: ")
+        print("[!] summary")
+        print(f"    hash: {imdb}")
+        print(f"    name: {name}")
+        print(f"    year: {year}")
+        print(f"    imdb: {hash}")
+        print(f"    imge: {poster}")
+
+        with open(poster, "rb") as image:
+            poster_bytes = image.read();
+
+        write = input("[?] write to db (y/n): ")
             if write == "y":
-                cur.execute("INSERT INTO torrents VALUES (?, ?, ?, ?, ?)", (hash, name, movi, year, imdb))
+                cur.execute("INSERT INTO movies VALUES (?, ?, ?, ?, ?)", (imdb, name, year, hash, poster_bytes))
                 con.commit()
-        elif movi == 0 or movi == "0":
-            print("[!] summary")
-            print(f"    hash: {hash}")
-            print(f"    name: {name}")
-            write = input("[?] write (y/n): ")
-            if write == "y":
-                cur.execute("INSERT INTO torrents VALUES (?, ?, ?, ?, ?)", (hash, name, movi, "", ""))
-                con.commit()
-        else:
-            print("\n[!] invalid input")
+            else:
+                print("\n[!] did not wrote to db")
     else:
-        print("\n[!] torrent allready exists")
+        print("\n[!] allready exists")
+        print(f"\n    imdb: {existing_row[0]}")
+        print(f"\n    name: {existing_row[1]}")
+        print(f"\n    year: {existing_row[2]}")
 
 def listAll():
-    for row in cur.execute("SELECT rowid, * FROM torrents").fetchall():
-        print(f"[{row[0]}]\t{row[1]}\t{row[2]}\t{row[3]}\t{row[4]}\t{row[5]}")
+    for row in cur.execute("SELECT rowid, imdb, name, year, hash FROM movies").fetchall():
+        print(f"[{row[0]}]\t{row[1]}\t{row[2]}\t{row[3]}\t{row[4]}")
 
 def deleteRow():
-    id = input("[?] id: ")
-    row = cur.execute("SELECT rowid, * FROM torrents WHERE rowid=?", (id,)).fetchone()
+    imdb = input("[?] imdb: ")
+    row = cur.execute("SELECT rowid, name, year FROM movies WHERE imdb=?", (imdb,)).fetchone()
     print("[!]")
-    print(f"[{row[0]}]\t{row[1]}\t{row[2]}\t{row[3]}\t{row[4]}\t{row[5]}")
+    print(f"[{row[0]}]\t{row[1]}\t{row[2]}")
     delete = input("[?] delete (y/n): ")
     if delete == "y":
-        cur.execute("DELETE FROM torrents WHERE rowid=?", (id,))
+        cur.execute("DELETE FROM movies WHERE imdb=?", (imdb,))
         con.commit()
+        print("[!] done")
+    else:
+        print("[!] did not deleted")
 
 def htmlOutput():
     for row in cur.execute("SELECT * FROM torrents WHERE isMovie=1").fetchall():
