df510fb550f733e882de9363d9726dc709adae56
tmash <132013534+7m45h@users.noreply.github.com>
2023-06-25 20:10:19 +0530

    workout great

--- scripts/scary_library.py
+++ scripts/scary_library.py
@@ -1,15 +1,23 @@
 #! /usr/bin/env python
 
 import json
+from urllib.parse import urlparse
 
 _input = "../warehouse/bookmarks.json"
+_output = """<ul id="list-web">"""
 
 with open(_input, 'r') as file:
     data = json.loads(file.read())
 
 for folder in data['children'][1]['children'][0]['children']:
-    print(folder['title'])
+    _output += f"<li>{folder['title']}</li><ul>"
     for item in folder['children']:
-        print(item['uri'])
+        _output += f"""<li><a class="anc-ext" href="{urlparse(item['uri']).scheme}://{urlparse(item['uri']).hostname}/" target="_blank">{urlparse(item['uri']).hostname} &#8599;</a></li>"""
+
+    _output += "</ul>"
+
+_output +="</ul>"
+
+print(_output)
 
 exit()
